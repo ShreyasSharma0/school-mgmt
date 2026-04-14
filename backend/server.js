929dc44233a6cb578cfa66b5c9570bc0cf685fd3
ShreyasSharma0 <157108495+ShreyasSharma0@users.noreply.github.com>
2026-04-14 19:27:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -96,10 +96,7 @@ const connectDB = async () => {
   }
 };
 
-const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
-});
+
+module.exports = app;
+
